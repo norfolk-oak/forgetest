a07ce13b6a6bd54de8f05328d4ba7fe856e06125
LSmithx2 <42276854+lsmithx2@users.noreply.github.com>
2022-04-19 13:13:43 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,20 +1,16 @@
 <!DOCTYPE html>
 <html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>IBCM | Access Forbidden</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link rel="stylesheet" href="./style.css">
-
-</head>
-<body>
-<!-- partial:index.partial.html -->
-<div class="container">
-	<div class="forbidden-sign"></div>
-	<h1>Access to this page is restricted.</h1>
-	<p>Ensure you have sufficient permissions to access the same.</p>
-</div>
-<!-- partial -->
-  
-</body>
+   <head>
+      <meta charset="UTF-8">
+      <title>IBCM | Access Forbidden</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+      <link rel="stylesheet" href="./style.css">
+   </head>
+   <body>
+      <div class="container">
+         <div class="forbidden-sign"></div>
+         <h1>Access to this page is restricted.</h1>
+         <p>Ensure you have sufficient permissions to access the same.</p>
+      </div>
+   </body>
 </html>
