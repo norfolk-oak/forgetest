ddc496a4ffb9ac35ae1816865c78733ad6e337d0
LSmithx2 <42276854+lsmithx2@users.noreply.github.com>
2022-04-19 13:12:17 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,27 +1,20 @@
-<?php
-
-$root = $_SERVER['DOCUMENT_ROOT'];
-$title = 'IBCM | Server Default Page';
-
-?>
 <!DOCTYPE html>
-<html lang="en">
-   <head>
-      <title><?php echo $title; ?></title>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="/style.css">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <style>
-      body 
-      {
-       font-family: "Lato", sans-serif
-      }
-      </style>
-   </head>
-   <body>
-     <h2><?php echo $title; ?></h2>
-     
-   </body>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>IBCM | Access Forbidden</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="container">
+	<div class="forbidden-sign"></div>
+	<h1>Access to this page is restricted.</h1>
+	<p>Ensure you have sufficient permissions to access the same.</p>
+</div>
+<!-- partial -->
+  
+</body>
 </html>
